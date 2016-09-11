b2abb2c1908c633d800a8f630dfebfb14f068642
Suresh Velusamy <suresh.v@compassitesinc.com>
2016-09-11 16:02:42 +0530

    welcome page typo fixed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
     <div class="content">
         <div class="title m-b-md">
-            Laravel Boilerplate Aapp
+            Laravel Boilerplate App
         </div>
 
         <div class="links">
